b82d3502dca1855d3b505f32fe0615ab1d7343e2
minechoo <choominhae@gmail.com>
2023-05-17 13:17:11 +0900

    scss 사용법

---
+++ main.js
@@ -0,0 +1,9 @@
+// const abc = () => {} =>
+// a === b;
+
+// a !== b
+// a >= b
+// a <= b;
+
+const items = document.querySelectorAll('.item');
+setTimeout(() => items.forEach((el) => el.classList.add('on')), 0);
